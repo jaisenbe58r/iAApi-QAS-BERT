19b09ebc4f7c0bd9156cc55ba406e92dd6eff446
Jaime Sendra <jsendra@autis.es>
2020-10-28 18:05:18 +0100

    result

--- streamlit/ui.py
+++ streamlit/ui.py
@@ -39,8 +39,9 @@ if st.button('Get Answering'):
 
     if user_input_context and user_input_question:
         result = process(user_input_context, user_input_question, backend)
-        st.write(f'Respuesta:    {result.content.answer}')
-        st.write(f'Score:        {result.content.score}')
+        result_json = json.loads(result.content)
+        st.write(f'Respuesta:    {result_json["answer"]}')
+        st.write(f'Score:        {result_json["score"]}')
 
     elif user_input_context:
         # handle case with no image
